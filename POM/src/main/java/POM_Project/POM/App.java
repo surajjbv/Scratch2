6bef691270871e55a059f6f901f588d47e4b18cf
surajjbv <surajjbv@gmail.com>
2017-07-26 16:03:26 -0700

    Suraj Name added

---
+++ POM/src/main/java/POM_Project/POM/App.java
@@ -0,0 +1,13 @@
+package POM_Project.POM;
+
+/**
+ * Hello world! suraj
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
